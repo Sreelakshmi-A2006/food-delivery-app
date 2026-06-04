ceffccad9a4328a2a7ee093b0c41155d4edc4bed
Rakesh <rakeshjack7@gmail.com>
2026-06-04 20:17:53 +0530

    good

---
+++ client/src/App.js
@@ -0,0 +1,55 @@
+import React, { useState, useEffect } from "react";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Cart from "./pages/Cart";
+import Orders from "./pages/Orders";
+
+function App() {
+    const [user, setUser] = useState(null);
+
+    useEffect(() => {
+        const storedUser = localStorage.getItem("user");
+        if (storedUser) {
+            setUser(JSON.parse(storedUser));
+        }
+    }, []);
+
+    const loginUser = (userData, token) => {
+        localStorage.setItem("user", JSON.stringify(userData));
+        localStorage.setItem("token", token);
+        setUser(userData);
+    };
+
+    const logoutUser = () => {
+        localStorage.removeItem("user");
+        localStorage.removeItem("token");
+        setUser(null);
+    };
+
+    return (
+        <Router>
+            <Navbar user={user} onLogout={logoutUser} />
+            <div className="container my-4">
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route 
+                        path="/login" 
+                        element={user ? <Navigate to="/" /> : <Login onLogin={loginUser} />} 
+                    />
+                    <Route 
+                        path="/register" 
+                        element={user ? <Navigate to="/" /> : <Register />} 
+                    />
+                    <Route path="/cart" element={<Cart />} />
+                    <Route path="/orders" element={<Orders />} />
+                    <Route path="*" element={<Navigate to="/" />} />
+                </Routes>
+            </div>
+        </Router>
+    );
+}
+
+export default App;
